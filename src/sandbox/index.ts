f6397e14fe3e7c02ecb74e18a54bb68f513caf21
daniel corcoran <danielc.92@outlook.com>
2023-05-04 21:05:53 +1000

    feat: update sandbox

--- src/sandbox/index.ts
+++ src/sandbox/index.ts
@@ -1,75 +1,125 @@
+import { hi } from './i';
+import { hid } from './i';
+
+class Dasdas {
+   constructor () {
+      
+   }
+}
+
 const foo = () => {
-  
-   const foo = 'bar';
+   const fofo = 'bar';
+   
+   const ff = '44';
+
+   const ff53 = 'f';
+
    const fobar = 'fobar';
- 
+
    if (fobar === fobar) {
- 
       console.log('fobor');
-   
    }
- 
+
+   const rrr = 'das';
+
+   function foo () {
+      const r = 444;
+   }
+
+   function food () {
+      const y = 555;
+   }
+
    const n = new Date().getDay();
-   switch(n) {
- 
-   case 2:
-      console.log('haha');
-      break;
-   case 5:
-      console.log('hahaa');
-      break;
- 
-   case 6:
-      console.log('hahadd');
-      break;
-   default: 
-      console.log('kh');
-  
+
+   switch (n) {
+      case 2:
+         console.log('haha'); 
+         break;
+      case 5:
+         console.log('hahaa');
+         break;
+      case 6:
+         console.log('hahadd');
+         break; 
+      default: 
+         console.log('kh');
    }
- 
+
    const arr = [ 1, 2, 3, 45, 6 ];
-   
+
    const badawat = async () => {
- 
+      const r = 'lol';
+
       return Promise.resolve(1654984);
-   
    };
-   
+
    const res = badawat();
+   
    const obj1 = { 'daniel': 'email@outloo' };
-     
+
+   const dddd = obj1;
+
    const N = 'Daniel';
  
    console.log(N);
+
+   const d = [ 'daniel', 'was' ];
+
    const unsort = {
+      
       'apple': 'banan',
+      
       'orange': 'orange',
    };
-   console.log(unsort);
+
    const fo = fobar;
+
    const bademail = 'danieloutlook.com';
-   const badphone = '0412331233';
- 
+
+   const rw = 'faf';
+
+   const r = rw;
+
+   const badphone = '04123333';
+
    const rr = typeof 333;
+
    const tern = fobar.length > 33
       ? 'tern'
       : 'terna';
- 
+
    const somefung = () => {
- 
       return 4;
-    
    };
-   if (tern.length > 0) {
- 
-      console.log(tern);
-      const err = new Error();
-      throw 'should ntoi be';
-   
+
+   const array = [ 1, 2, 3, 4, 445, 555 ];
+
+   try {
+      const r = 34234;
+   } catch (e) {
+      console.log(e);
    }
- 
+
+   while (array.length > 0) {
+      array.push(1234);
+   }
+
+   for ( let index = 0; index < array.length; index++) {
+      const element = array[index];
+      
+      return element + 44;
+   }
+
+   if (tern.length > 0) {
+      const err = new Error();
+
+      throw 'should ntoi be';
+   }
 };
- 
+
 const nosemi = 'nosemi';
  
 console.log(nosemi);
+
+export {};
